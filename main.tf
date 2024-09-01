6a6d481c9383e5b67f7b745c627d01ca7db7564b
researchdevelopment <fotouhiarian@gmail.com>
2024-08-31 22:10:10 -0400

    v1.5

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "my_bucket" {
   name          = "tt-githubactions-bucket"
-  project       = "My Project 12540"
+  project       = "omega-winter-434300-d8"
   location      = "US"
   force_destroy = true
 
